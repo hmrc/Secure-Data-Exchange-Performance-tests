a3dde2a3ca7cab68554915ae7a80d80ec01fd2d1
danhamilton <4736395+danhamilton@users.noreply.github.com>
2025-01-13 17:14:34 +0000

    Rm non-used tests

--- src/test/scala/uk/gov/hmrc/perftests/api/KeyRotationApiSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/api/KeyRotationApiSimulation.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.perftests.api
 
 import io.gatling.core.Predef.exec
 import uk.gov.hmrc.performance.api.APIPerformanceTest
-import uk.gov.hmrc.performance.api.models.PerformanceTest
+import uk.gov.hmrc.performance.api.models.{PerformanceTest, PerformanceTestConfiguration}
 import uk.gov.hmrc.performance.conf.JourneyConfiguration
 import uk.gov.hmrc.perftests.requests.KeyRotationApiRequests.{requestDeleteKeyAPiUrlRequest, requestGetSdesPublicKeyApiRequest, requestPostKeyAPiUrlRequest}
 
@@ -31,5 +31,14 @@ class KeyRoationApiSimulation extends APIPerformanceTest with JourneyConfigurati
     scope = "write:transfer-complete", // scope required despite not being used by api
     feeder = "data/helloworld.csv" // use dummy feeder
   )
+
+  override protected def runSimulation(privileged: Boolean = false, performanceTestConfiguration: PerformanceTestConfiguration): SetUp = {
+    if (journeysAvailable.contains(performanceTest.title)) {
+      super.runSimulation()
+    } else {
+      setUp(Seq.empty: _*)
+    }
+  }
+
   runSimulation()
 }
